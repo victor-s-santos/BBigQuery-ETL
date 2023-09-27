34c454026a258f7b9c08852faee7284559a86de7
Victor <victor.santos.cd@gmail.com>
2023-09-27 12:44:33 -0300

    (db_connection/db_connector.py): Creating the mysql connector object

--- db_connection/db_connector.py
+++ db_connection/db_connector.py
@@ -15,7 +15,9 @@ class MySQLConnection:
                 database = self.dict_credentials["DB_NAME"],
                 port = self.dict_credentials["PORT"]
             )
-            return db
+            my_cursor = db.cursor()
+            my_cursor.execute("SELECT * FROM sample")
+            return my_cursor.fetchone()
         except Exception as e:
             print(f"An error has been occured: {e}")
     
@@ -23,16 +25,17 @@ class MySQLConnection:
 
 if __name__ == "__main__":
     db_credentials = {
-    "HOST": "localhost",
-    "USER": "usuario",
-    "PASSWORD": "senha",
-    "DB_NAME": "base_de_dados",
-    "PORT": 3306
+        "HOST": "localhost",
+        "USER": "usuario",
+        "PASSWORD": "senha",
+        "DB_NAME": "base_de_dados",
+        "PORT": 3306
     }
 
     try:
         db = MySQLConnection(dict_credentials=db_credentials)
-        print("Everything is OK!")
+        receive_obj = db.connect_object()
+        print(receive_obj)
     except Exception as e:
         print(f"An error: {e}")
 
